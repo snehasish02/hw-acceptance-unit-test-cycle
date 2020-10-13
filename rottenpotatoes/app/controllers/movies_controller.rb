354513d6b9bfc0ca22395f75d0e0fe356a8bd033
Snehasish Sen <sen.snehasish02@gmail.com>
2020-10-13 01:20:35 +0000

    Part 1 done

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
@@ -60,5 +60,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
+  
 end
